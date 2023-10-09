dc5bda095b378b69586f4a61961b00741fd3e08d
Oluwatimilehin <odebodezion@gmail.com>
2023-10-09 17:33:45 +0100

    feat: Add the isMtn method to verify if number is mtn number

--- src/script.js
+++ src/script.js
@@ -33,33 +33,42 @@ export function ValidateForm (phoneNumber) {
         glo: [
             '805', '807', '705', '815', '811', '905', "915"
         ]
-
+        }
+        this.getPrefixes = function () {
+            return prefixes
+        }
     }
-
-}
-
-ValidateForm.prototype.validateIfNumbersOnly = function () {
-    // this function validates that only numbers are in the input field
-     
-    return /^[^A-Za-z]+$/.test(this.phoneNumber)
-}
-
-ValidateForm.prototype.getPhoneNumber = function () {
-    console.log(this.phoneNumber)
-}
-
-
-
-/*
-function MTN () {
     
-}   
-
-function Airtel () {
-
-}
-+
-function Glo () {}
-
-function Etisalat () {}
-*/
+ValidateForm.prototype.validateIfNumbersOnly = function () {
+        // this function validates that only numbers are in the input field
+         
+        return /^[^A-Za-z]+$/.test(this.phoneNumber)
+    }
+    
+ValidateForm.prototype.getPrefixFromPhoneNumber = function () {
+        // this function returns the numbers between index 1 to 4 
+        // which reps the prefix of the number
+        if (this.phoneNumber.includes('+')) {
+            return this.phoneNumber.slice(4,7)
+        }
+        return this.phoneNumber.slice(1,4)
+    }
+    
+ValidateForm.prototype.getPhoneNumber = function () {
+        console.log(this.phoneNumber)
+    }
+    
+ValidateForm.prototype.isMtnNumber = function () {
+        // this function check if this.phoneNumber is an MTN number by checking
+        // against the prefixes
+        const mtnPrefixes = this.getPrefixes().mtn;
+        let numberPrefix = this.getPrefixFromPhoneNumber(this.phoneNumber);
+        if (mtnPrefixes.includes(numberPrefix)) {
+            return true
+        };
+        return false
+    }
+    
+ValidateForm.prototype.isGloNumber = function () {
+    
+    }
