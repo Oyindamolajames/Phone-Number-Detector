e7e95f6e8af4acaf931b974aa51a94fa37485061
Oluwatimilehin <odebodezion@gmail.com>
2023-10-09 17:30:58 +0100

    Add prefixes object for different carriers

--- src/script.js
+++ src/script.js
@@ -4,6 +4,38 @@ export function ValidateForm (phoneNumber) {
         return new ValidateForm(phoneNumber)
     }
     this.phoneNumber = phoneNumber;
+    const prefixes = {
+        mtn: ['803', 
+        '806', 
+        '810', 
+        '813',
+        '814',
+        '816',
+        '903',
+        '7025',
+        '7026',
+        '703',
+        '706',
+        '913',
+        '916',
+        '704',
+        '906'],
+
+        airtel: [
+            '701', '708', '802', '808', '812', '901', '902',
+            '904', '907', '912',            
+        ],
+
+        nineMobile: [
+            '809', '818', '817', '909' , '908'
+        ],
+
+        glo: [
+            '805', '807', '705', '815', '811', '905', "915"
+        ]
+
+    }
+
 }
 
 ValidateForm.prototype.validateIfNumbersOnly = function () {
