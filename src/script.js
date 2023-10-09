cc1a30e857f35061bcfc6f9e3a9434c37f898e4f
Oluwatimilehin <odebodezion@gmail.com>
2023-10-09 16:21:39 +0100

    feat: Add ValidateForm class with phoneNumber validation
    - Add a new ValidateForm class to handle phone number validation.
    - Implement a validateIfNumbersOnly method to check if the input contains only numbers.
    - Add a getPhoneNumber method for debugging purposes.

--- src/script.js
+++ src/script.js
@@ -1,17 +1,34 @@
 // 
 
-export function NetworkProvider (number) {
-    this.number = 
+export function ValidateForm (phoneNumber) {
+    if (this instanceof ValidateForm === false) {
+        return new ValidateForm(phoneNumber)
+    }
+    this.phoneNumber = phoneNumber;
 }
 
+ValidateForm.prototype.validateIfNumbersOnly = function () {
+    // this function validates that only numbers are in the input field
+     
+    return /^[^A-Za-z]+$/.test(this.phoneNumber)
+}
+
+ValidateForm.prototype.getPhoneNumber = function () {
+    console.log(this.phoneNumber)
+}
+
+
+
+/*
 function MTN () {
-
-}
+    
+}   
 
 function Airtel () {
 
 }
-
++
 function Glo () {}
 
 function Etisalat () {}
+*/
